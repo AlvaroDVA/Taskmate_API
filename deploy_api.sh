32ff9e64b5f202b198fc0609657e5add0c7daec2
Alvaro Del Val Arce <alvarodelvalarce@hotmail.com>
2024-05-01 17:23:15 +0200

    Fixes Deploy

--- deploy_api.sh
+++ deploy_api.sh
@@ -9,32 +9,30 @@ show_error_and_exit() {
 # Actualizar el sistema
 sudo apt update && sudo apt upgrade -y || show_error_and_exit "No se pudo actualizar el sistema"
 
-# Instalar las dependencias necesarias
-sudo apt install -y python3-pip || show_error_and_exit "No se pudieron instalar las dependencias de Python"
-
-# Instalar las dependencias de Python
-pip3 install fastapi uvicorn gunicorn || show_error_and_exit "No se pudieron instalar las dependencias de Python"
+# Instalar python3-venv
+sudo apt install -y python3-venv || show_error_and_exit "No se pudo instalar python3-venv"
 
 # Directorio de destino
 API_DIR="/var/www/api"
 
-# Clonar el repositorio si no existe
-if [ ! -d "$API_DIR" ]; then
-    git clone https://github.com/AlvaroDVA/Taskmate_API.git "$API_DIR" || show_error_and_exit "No se pudo clonar el repositorio"
-else
-    # Si el repositorio ya existe, actualizarlo si hay cambios
-    cd "$API_DIR" || show_error_and_exit "No se pudo cambiar al directorio del repositorio"
-    git pull origin master >/dev/null 2>&1
-    if [ $? -ne 0 ]; then
-        show_error_and_exit "No se pudo actualizar el repositorio"
-    fi
+# Eliminar el directorio existente si hay contenido
+if [ -d "$API_DIR" ]; then
+    sudo rm -rf "$API_DIR" || show_error_and_exit "No se pudo eliminar el directorio existente"
 fi
 
-# Cambiar al directorio de la API
-cd "$API_DIR" || show_error_and_exit "No se pudo cambiar al directorio de la API"
+# Clonar el repositorio desde cero
+git clone https://github.com/AlvaroDVA/Taskmate_API.git "$API_DIR" || show_error_and_exit "No se pudo clonar el repositorio"
+
+# Crear y activar un entorno virtual
+VENV_DIR="$API_DIR/venv"
+python3 -m venv "$VENV_DIR" || show_error_and_exit "No se pudo crear el entorno virtual"
+source "$VENV_DIR/bin/activate" || show_error_and_exit "No se pudo activar el entorno virtual"
+
+# Instalar las dependencias de Python
+pip install fastapi uvicorn gunicorn || show_error_and_exit "No se pudieron instalar las dependencias de Python"
 
 # Iniciar Gunicorn
-nohup gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app -b 0.0.0.0:15556 & || show_error_and_exit "No se pudo iniciar Gunicorn"
+nohup gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app -b 0.0.0.0:15556 &
 
 # Crear un archivo de servicio para systemd
 sudo tee /etc/systemd/system/fastapi.service >/dev/null <<EOF || show_error_and_exit "No se pudo crear el archivo de servicio"
@@ -46,15 +44,13 @@ After=network.target
 User=taskmate
 Group=taskmate
 WorkingDirectory=$API_DIR
-ExecStart=/usr/local/bin/gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app -b 0.0.0.0:15556
+ExecStart=$VENV_DIR/bin/gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app -b 0.0.0.0:15556
 Restart=always
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-# Habilitar y arrancar el servicio
-sudo systemctl daemon-reload && sudo systemctl enable fastapi && sudo systemctl start fastapi || show_error_and_exit "No se pudo habilitar y arrancar el servicio"
+nohup gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app -b 0.0.0.0:15556 &
 
 echo "El script se ha ejecutado correctamente."
-
